4444c943b38febe4f5906a59807947f129aea63f
shyptia <shyptianastya27@gmail.com>
2024-02-27 09:55:32 +0200

    fix error in the jobs page

--- pages/jobs.tsx
+++ pages/jobs.tsx
@@ -1,5 +1,10 @@
 import { JobsPage } from "@/features/pages/JobsPage";
+import { useRouter } from "next/router";
 
 export default function Page() {
+  const router = useRouter();
+
+  if (Object.keys(router.query).length !== 0 && !router.isReady) return null;
+
   return <JobsPage />;
 }
